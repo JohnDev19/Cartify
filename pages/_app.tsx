faf5b8a2748839a562fcb3330745d580827aa358
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-11-29 11:34:08 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import { CartProvider } from '../app/contexts/CartContext'
+import { Toaster } from "../components/ui/toaster"
 import '../app/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
       <Component {...pageProps} />
+      <Toaster />
     </CartProvider>
   )
 }
